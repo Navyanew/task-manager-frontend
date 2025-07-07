9719b4cd63bc65110da7c323d2f7ce5292f8f9dd
Navya <prathigudupunavyasri@gmail.com>
2025-07-07 21:47:37 +0100

    Updated API baseURL to deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // your backend base URL
+  baseURL:'https://task-manager-backend-hnvj.onrender.com/api', // your backend base URL
 });
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
